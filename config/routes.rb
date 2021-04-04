48b7450d54ce23bce6b398393680e975f2ad1952
shunsuke-oka <71703108+shunsuke-oka@users.noreply.github.com>
2021-04-05 02:35:13 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
   root to: 'tweets#index'
-<<<<<<< Updated upstream
-  resources :tweets, only: [:index, :new, :create, :destroy]
-=======
   resources :tweets, only: [:index, :new, :create, :destroy, :edit]
->>>>>>> Stashed changes
 end
